ffd8b6ff1bb45176ce7472dcc13789a5dbb07ea5
Micah Weiberg <micah.weiberg@gmail.com>
2019-07-03 15:27:02 -0500

    Updating more HTML

--- app/static/shift_buttons.js
+++ app/static/shift_buttons.js
@@ -1,27 +1,5 @@
-// Not currently working
-
 $(document).ready(function() {
-    let input = "";
-
-    // RFID scanner info
-    $(document).on('keydown', function(key) {
-        if(key.keyCode == 13) {
-            let url = "{{ url_for('ShiftsView:verify_scanner') }}";
-            let scannedInput = {
-                'scan': input
-            };
-            $.post(url, scannedInput, function(success) {
-                if(success != 'failed') {
-                    window.location.replace("{{ lab_base_url }}/session/no-cas/checkin/{{ session_info.id }}/{{ session_info.hash }}/" + success);
-                } else {
-                    input = "";
-                }
-            });
-        } else {
-            input = input + key.key;
-        }
-    });
-
+    // Currently does nothing but display an in-browser alert when clicked
     $("#generate-shifts").click(function() {
        let input_data = {'clicked': 'true'};
        $.post('/generate_shifts', input_data, function(output_data) {
--- app/views.py
+++ app/views.py
@@ -67,77 +67,14 @@ class ShiftsView(FlaskView):
         self.sc.check_roles_and_route(['Administrator'])
         return render_template('users/add_user.html', **locals())
 
-    @route('/users/admin/<int:user_id>')
-    def edit_user(self, user_id):
-        self.sc.check_roles_and_route(['Administrator'])
-        user = self.user.get_user(user_id)
-        roles = self.user.get_all_roles()
-        user_role_ids = self.user.get_user_role_ids(user_id)
-        return render_template('users/edit_user.html', **locals())
-
-    @route('/users/create/<username>/<first_name>/<last_name>')
-    def select_user_roles(self, username, first_name, last_name):
-        self.sc.check_roles_and_route(['Administrator'])
-        roles = self.user.get_all_roles()
-        existing_user = self.user.get_user_by_username(username)
-        if existing_user:  # User exists in system
-            message = 'This user already exists in the system and is activated.'
-        return render_template('users/select_user_roles.html', **locals())
-
-    @route('/users/search-users', methods=['POST'])
-    def search_users(self):
-        self.sc.check_roles_and_route(['Administrator'])
-        form = request.form
-        first_name = form.get('firstName')
-        last_name = form.get('lastName')
-        # results = self.wsapi.get_username_from_name(first_name, last_name)
-        return render_template('users/user_search_results.html', **locals())
-
-    @route('users/save-user-edits', methods=['POST'])
-    def save_user_edits(self):
-        self.sc.check_roles_and_route(['Administrator'])
-        form = request.form
-        user_id = form.get('user-id')
-        username = form.get('username')
-        first_name = form.get('first-name')
-        last_name = form.get('last-name')
-        roles = form.getlist('roles')
-        try:
-            self.user.update_user_info(user_id, first_name, last_name)
-            self.user.clear_current_roles(user_id)
-            self.user.set_user_roles(username, roles)
-            self.slc.set_alert('success', 'Edited {0} {1} ({2}) successfully!'.format(first_name, last_name, username))
-            return redirect(url_for('ShiftsView:users_index'))
-        except Exception as error:
-            self.slc.set_alert('danger', 'Failed to edit user: {0}'.format(str(error)))
-            return redirect(url_for('ShiftsView:edit_user', user_id=user_id))
-
-    @route('/users/create-users', methods=['POST'])
-    def create_user(self):
-        self.slc.check_roles_and_route(['Administrator'])
-        form = request.form
-        first_name = form.get('first-name')
-        last_name = form.get('last-name')
-        username = form.get('username')
-        roles = form.getlist('roles')
-        try:
-            self.user.create_user(first_name, last_name, username)
-            self.user.set_user_roles(username, roles)
-            self.slc.set_alert('success', '{0} {1} ({2}) added successfully!'.format(first_name, last_name, username))
-            return redirect(url_for('ShiftsView:users_index'))
-        except Exception as error:
-            self.slc.set_alert('danger', 'Failed to add user: {0}'.format(str(error)))
-            return redirect(url_for('ShiftsView:select_user_roles', username=username, first_name=first_name,
-                                    last_name=last_name))
-
     # OTHER #
 
-    @route('/no-cas/verify-scanner', methods=['post'])
-    def verify_scanner(self):
-        form = request.form
-        scan = form.get("scan")
-        card_id = re.search("\[\[(.+?)\]\]", scan)
-        if card_id:
-            return card_id.group(1)
-        else:
-            return 'failed'
+    # @route('/no-cas/verify-scanner', methods=['post'])
+    # def verify_scanner(self):
+    #     form = request.form
+    #     scan = form.get("scan")
+    #     card_id = re.search("\[\[(.+?)\]\]", scan)
+    #     if card_id:
+    #         return card_id.group(1)
+    #     else:
+    #         return 'failed'
