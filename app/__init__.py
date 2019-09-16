bcf97ac9b877fa288a6738bfcc12867a89079e7e
Bailey Middendorf <bam95899@bethel.edu>
2019-09-16 08:59:05 -0500

    removed sentry legacy code from base level init

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ import logging
 
 # Packages
 from flask import Flask
-from raven.contrib.flask import Sentry
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -10,6 +9,5 @@ app.config.from_object('config')
 # Declaring and registering the view
 from app.views import ShiftsView
 
-sentry = Sentry(app, dsn=app.config['SENTRY_URL'], logging=True, level=logging.ERROR)
 
 ShiftsView.register(app, route_base='/')
