f65b548d506740156138e1d06686a314352345c3
Bailey Middendorf <bam95899@bethel.edu>
2019-09-16 08:59:16 -0500

    Added sentry_sdk import to base level init

--- app/__init__.py
+++ app/__init__.py
@@ -9,5 +9,9 @@ app.config.from_object('config')
 # Declaring and registering the view
 from app.views import ShiftsView
 
+if app.config['ENVIRON'] == 'prod' and app.config['SENTRY_URL']:
+    import sentry_sdk
+    from sentry_sdk.integrations.flask import FlaskIntegration
+    sentry_sdk.init(dsn=app.config['SENTRY_URL'], integrations=[FlaskIntegration()])
 
 ShiftsView.register(app, route_base='/')
